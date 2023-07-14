0f43a50a446d1adc3d03d0d2b306f9607248eb54
Dave <8709533+DaveBathnes@users.noreply.github.com>
2023-07-14 22:36:22 +0100

    Refining city classification

--- db/vw_areas.sql
+++ db/vw_areas.sql
@@ -5,6 +5,7 @@ select
     string_agg(ba.auth_name, '/') as auth_names,
     geom,
     population,
+    classification,
     min(min_imd_decile) as min_imd_decile
 from (
     select 
@@ -12,7 +13,17 @@ from (
         a.name as name,
         a.geom as geom,
         min(imd.imd_decile) as min_imd_decile,
-        sum(p.population) as population
+        sum(p.population) as population,
+        case
+            when sum(p.population) is null then 'Villages and small communities'
+            when sum(p.population) < 7500 then 'Villages and small communities'
+            when sum(p.population) >= 7500 and sum(p.population) < 25000 then 'Small Towns'
+            when sum(p.population) >= 25000 and sum(p.population) < 60000 then 'Medium Towns'
+            when sum(p.population) >= 60000 and sum(p.population) < 175000 then 'Large Towns'
+            when sum(p.population) >= 175000 and sum(p.population) < 500000 then 'Cities'
+            when sum(p.population) >= 500000 then 'Core cities'
+            else null 
+        end as classification
     from areas a
     left join oa_bua o
     on o.bua = a.gsscode
@@ -24,4 +35,4 @@ from (
     group by a.gsscode, a.name, a.geom) ars
 join bua_auth ba
 on ba.bua = ars.code
-group by code, name, geom, population;
+group by code, name, geom, population, classification;
